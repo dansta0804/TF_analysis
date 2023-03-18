5055b54a2b6dbf70bbe2eeeaa854ea2e855434e6
Danielė <97364205+dansta0804@users.noreply.github.com>
2023-03-18 21:41:01 +0200

    Created file that stores functions.

---
+++ functions.R
@@ -0,0 +1,47 @@
+# nolint start
+# A function that reads PWM of certain transcription factor:
+get_PWM <- function(pwm_name) {
+    TF_pwm <-
+        read.table(file = paste0(INTER_FILES, pwm_name)) %>%
+        t() %>%
+        `rownames<-`(c("A", "C", "G", "T"))
+    return(TF_pwm)
+}
+
+# A function that annotates peaks by assigning gene id and gene symbol:
+annotate_peaks <- function(peak_set, genome, known_genes, anno_db) {
+    grl_annotation <- GRangesList()
+    for (object in 1:length(peak_set)) {
+        name <- gsub(" ", "_", names(peak_set[object]))
+        peak <- peak_set[[object]]
+        seqlengths(peak) <- seqlengths(peak) - 10001
+        anno_db <- "org.Mm.eg.db"
+        peak_annotation <-
+            annotatePeak(peak, tssRegion = c(-3000, 3000), TxDb = known_genes,
+            annoDb = anno_db)
+
+        annotation <- as.data.frame(peak_annotation@anno)
+        entrezids <- unique(annotation$geneId)
+        entrez2gene <-
+            genome %>%
+            filter(entrez %in% entrezids) %>%
+            dplyr::select(entrez, symbol)
+
+        m <- match(mm_annot$geneId, entrez2gene$entrez)
+        organism_annot <-
+            cbind(mm_annot[, 1:14], gene_symbol = entrez2gene$symbol[m],
+                  mm_annot[, 15:16])
+
+        # Defining an object that has two extra columns with gene id and symbol:
+        grl_annotation[[object]] <- organism_annot
+        names(grl_annotation)[object] <- names(grl_annotation)[object]
+    }
+    return(grl_annotation)
+}
+
+
+
+
+
+
+# nolint end
