842c500bbf8b26ecd133e4463192c52073bda9ed
Danielė <97364205+dansta0804@users.noreply.github.com>
2023-03-29 22:30:55 +0300

    Added new libraries (some may not be used).

--- Scripts/App/app.r
+++ Scripts/App/app.r
@@ -1,7 +1,10 @@
 # nolint start
 
-library(shiny)
-library(ggplot2)
+
+library(pacman)
+p_load(shiny, data.table, rtracklayer, randomcoloR, ggplot2, ggthemes,
+       plyranges, ggpubr, BRGenomics, reshape2, plotly, heatmaply,
+       dplyr, gplots, genomation, Biostrings, scales, GenomicRanges)
 
 PROJECT <- "/home/daniele/Desktop/IV_course/II_semester/TF_analysis/"
 source(paste0(PROJECT, "Scripts/App/ui.R"))
