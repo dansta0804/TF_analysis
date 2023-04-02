416bf621cca1d2cf195cdeb1c903f659cff1829d
Danielė <97364205+dansta0804@users.noreply.github.com>
2023-04-02 19:39:13 +0300

    Removed unneeded library.

--- Scripts/App/app.r
+++ Scripts/App/app.r
@@ -2,9 +2,9 @@
 
 
 library(pacman)
-p_load(shiny, data.table, rtracklayer, randomcoloR, ggplot2, ggthemes,
-       plyranges, ggpubr, BRGenomics, reshape2, plotly, heatmaply,
-       dplyr, gplots, genomation, Biostrings, scales, GenomicRanges)
+p_load(shiny, data.table, rtracklayer, ggplot2, ggthemes, plyranges, ggpubr,
+       BRGenomics, reshape2, plotly, heatmaply, dplyr, gplots, genomation,
+       Biostrings, scales, GenomicRanges)
 
 PROJECT <- "/home/daniele/Desktop/IV_course/II_semester/TF_analysis/"
 source(paste0(PROJECT, "Scripts/App/ui.R"))
