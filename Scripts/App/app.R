f465d4605eb5fa361a891bddd31813e98ea3d636
Danielė <97364205+dansta0804@users.noreply.github.com>
2023-04-15 23:24:58 +0300

    Added new libraries.

--- Scripts/App/app.R
+++ Scripts/App/app.R
@@ -3,11 +3,14 @@
 library(pacman)
 p_load(shiny, data.table, rtracklayer, ggplot2, ggthemes, plyranges, ggpubr,
        BRGenomics, reshape2, plotly, heatmaply, dplyr, gplots, genomation,
-       Biostrings, scales, GenomicRanges, DT)
+       Biostrings, scales, GenomicRanges, DT, shinythemes, shinycustomloader)
 
 PROJECT <- "/home/daniele/Desktop/IV_course/II_semester/TF_analysis/"
-source(paste0(PROJECT, "Scripts/App/ui.R"))
+source(paste0("/home/daniele/Desktop/IV_course/II_semester/",
+"TF_analysis/Scripts/App/ui.R"))
 source(paste0(PROJECT, "Scripts/App/server.R"))
+# source(paste0(PROJECT, "Scripts/App/data_quality.R"))
+# source(paste0(PROJECT, "Scripts/App/data_analysis.R"))
 shinyApp(ui = ui, server = server)
 
 # nolint end
